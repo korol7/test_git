635e5c4b96a39d8962efb027805941d3b1a314c5
korol7 <korol7@163.com>
2020-03-19 12:57:01 +0800

    your comment

--- config.py
+++ config.py
@@ -7,17 +7,3 @@ pass
 class DevConfig(Config):
 DEBUG = True
 
-main.py:
-
-from flask import Flask
-from config import DevConfig
-
-app = Flask(__name__)
-app.config.from_object(DevConfig)
-
-@app.route('/')
-def home():
-return '<h1>Hello World!</h1>'
-
-if __name__ == '__main__':
-app.run()
